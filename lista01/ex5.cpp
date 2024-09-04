f2320053986c34eb35498176b897240e27f2812b
JvAlm71 <victoralmeida71@hotmail.com>
2024-09-04 14:14:12 -0300

    Exercicio 5, imprimindo certo...

--- lista01/ex5.cpp
+++ lista01/ex5.cpp
@@ -7,6 +7,7 @@ int main() {
     //numero maximo de frequencia
     const int maxfreq = 100000; 
     int freq[maxfreq] = {0}; 
+    bool elemento_impresso = false;
 
     for (int i = 0; i < n; ++i) {
         std::cin >> vet[i]; 
@@ -19,8 +20,14 @@ int main() {
     for (int i = 0; i < maxfreq; ++i) {
         if (freq[i] % 2 != 0) {
             std::cout << i << std::endl;
+            elemento_impresso = true;
         }
     }
+
+    if (!elemento_impresso) {
+        std::cout << 0 << std::endl;
+    }
+    
     delete[] vet;
     return 0;
 }
