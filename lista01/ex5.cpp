7e89a4a2200545a7e43e6fe185d1e331020c2a3b
Joao <victoralmeida71@usp.br>
2024-09-26 17:02:52 -0300

    mudando...Desisto

--- lista01/ex5.cpp
+++ lista01/ex5.cpp
@@ -1,29 +1,5 @@
 #include <iostream>
 
-void funcvetor(int vetor[], int n) {
-    //int n = 7;
-    //std::cin >> n;
-    const int MAX_VAL = 1000000;
-    int vetor[MAX_VAL];
-    // int count = 0;
-    int count[MAX_VAL + 1] = {0};
-
-    for (int i = 0; i < n; i++) {
-        std::cin >> vetor[i];  
-        count[vetor[i]]++;
-    }
-
-     for (int i = 0; i < n; i++) {
-        if (count[vetor[i]] % 2 != 0) {  
-            std::cout << vetor[i] << " ";  
-            count[vetor[i]] = 0;  
-        }
-    }
-
-    std::cout << std::endl;
-}
-
-/*
 int main() {
     int n = 7;
     //std::cin >> n;
@@ -47,4 +23,4 @@ int main() {
     std::cout << std::endl;
 
     return 0;
-}*/
+}
