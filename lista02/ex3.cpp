7ddcd02c7b803c5dd81f9d5b53ffb9c293ef20aa
JvAlm71 <victoralmeida71@usp.br>
2024-10-15 11:27:05 -0300

    colocando mais um Ex

---
+++ lista02/ex3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+
+class Vetor {
+private:
+    int _x;
+    int _y;
+
+public:
+    Vetor(int x = 0, int y = 0) : _x(x), _y(y) {}
+
+    // conceitos de sobrecargas
+    friend Vetor operator+(const Vetor &a, const Vetor &b) {
+        return Vetor(a._x + b._x, a._y + b._y);
+    }
+
+    friend Vetor operator-(const Vetor &a, const Vetor &b) {
+        return Vetor(a._x - b._x, a._y - b._y);
+    }
+
+    friend int operator*(const Vetor &a, const Vetor &b) {
+        return a._x * b._x + a._y * b._y;
+    }
+
+    friend int operator^(const Vetor &a, const Vetor &b) {
+        return a._x * b._y - a._y * b._x;
+    }
+
+    friend double operator<(const Vetor &a, const Vetor &b) {
+        double dotProduct = a * b;
+        double magnitudes = a.abs() * b.abs();
+        if (magnitudes == 0) return 0.0;
+        double cosTheta = dotProduct / magnitudes;
+        return acos(cosTheta) * (180.0 / M_PI); // conversão para graus
+    }
+
+    // metodo para calcular o modulo do vetor
+    double abs() const {
+        return sqrt(_x * _x + _y * _y);
+    }
+
+    // sobrecarga do operador para a saida no formato (x, y)
+    friend std::ostream& operator<<(std::ostream &out, const Vetor &v) {
+        out << "(" << v._x << ", " << v._y << ")";
+        return out;
+    }
+};
+
+int main() {
+    Vetor v;
+    std::cout << v << std::endl;
+    v = v + Vetor(1, 2);
+    std::cout << v << std::endl;
+    v = v + Vetor(1, 2);
+    std::cout << v << std::endl;
+    v = v - Vetor(1, 6);
+    std::cout << v << std::endl;
+    int a = v * Vetor(3, 2);
+    std::cout << a << std::endl;
+    //Vetor b = v ^ Vetor(-4501, 9002);
+    int  b = v ^ Vetor(-4501, 9002); // aqui o jeito correto para a formatacao
+    std::cout << b << std::endl;
+    double c = v.abs();
+    std::cout << c << std::endl;
+
+    return 0;
+}
