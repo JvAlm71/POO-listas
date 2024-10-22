a771f862d623d0d7ca749f32b57219289f5d5885
Joao <victoralmeida71@usp.br>
2024-10-22 10:57:28 -0300

    ex7 add

--- lista02/ex7.cpp
+++ lista02/ex7.cpp
@@ -1,126 +1,102 @@
 #include <iostream>
-#include <string>
+#include <numeric> 
 
-class BigInt {
-public:
-    BigInt(long long value = 0) : value_(std::to_string(value)) {}
-    BigInt(const std::string &value) : value_(value) {}
-
-    BigInt operator+(const BigInt &other) const { return BigInt("0"); } // Mock addition
-    BigInt operator-(const BigInt &other) const { return BigInt("0"); } // Mock subtraction
-    BigInt operator*(const BigInt &other) const { return BigInt("0"); } // Mock multiplication
-    BigInt operator/(const BigInt &other) const { return BigInt("0"); } // Mock division
-
-    bool operator==(const BigInt &other) const { return value_ == other.value_; }
-    bool operator!=(const BigInt &other) const { return !(*this == other); }
-
-    // Operador de comparação <
-    bool operator<(const BigInt &other) const {
-        // Comparação de tamanhos e lexicográfica para simular inteiros
-        if (value_.size() != other.value_.size()) {
-            return value_.size() < other.value_.size();
-        }
-        return value_ < other.value_;
-    }
-
-    // Operador de comparação >
-    bool operator>(const BigInt &other) const {
-        return other < *this;
-    }
-
-    // Operador de comparação <=
-    bool operator<=(const BigInt &other) const {
-        return !(*this > other);
-    }
-
-    // Operador de comparação >=
-    bool operator>=(const BigInt &other) const {
-        return !(*this < other);
-    }
-
-    friend std::ostream &operator<<(std::ostream &out, const BigInt &b) { return out << b.value_; }
-
+class C {
 private:
-    std::string value_; // Armazenado como string para representar valores grandes
-};
-
-class Fracao {
-private:
-    BigInt _numerador;
-    BigInt _denominador;
-
-    BigInt mdc(const BigInt &a, const BigInt &b) const {
-        return BigInt(1); // Placeholder para o MDC
-    }
-
-    void reduz() {
-        BigInt divisor = mdc(_numerador, _denominador);
-        _numerador = _numerador / divisor;
-        _denominador = _denominador / divisor;
-        if (_denominador != BigInt(0) && _denominador < BigInt(0)) {
-            _numerador = _numerador * BigInt(-1);
-            _denominador = _denominador * BigInt(-1);
-        }
-    }
+    double _real;
+    double _imag;
 
 public:
-    Fracao(BigInt numerador = BigInt(0), BigInt denominador = BigInt(1))
-        : _numerador(numerador), _denominador(denominador) {
-        if (_denominador == BigInt(0)) {
-            _denominador = BigInt(1);
-        }
-        reduz();
+    C(double r = 0.0, double i = 0.0) : _real(r), _imag(i) {}
+
+    double getReal() const { return _real; }
+    double getImag() const { return _imag; }
+
+    void setReal(double r) { _real = r; }
+    void setImag(double i) { _imag = i; }
+
+    virtual void print(std::ostream &out) const {
+        out << _real << " + " << _imag << "i";
     }
 
-    friend Fracao operator+(const Fracao &a, const Fracao &b) {
-        BigInt numerador = a._numerador * b._denominador + b._numerador * a._denominador;
-        BigInt denominador = a._denominador * b._denominador;
-        return Fracao(numerador, denominador);
-    }
-
-    friend Fracao operator-(const Fracao &a, const Fracao &b) {
-        BigInt numerador = a._numerador * b._denominador - b._numerador * a._denominador;
-        BigInt denominador = a._denominador * b._denominador;
-        return Fracao(numerador, denominador);
-    }
-
-    friend Fracao operator*(const Fracao &a, const Fracao &b) {
-        BigInt numerador = a._numerador * b._numerador;
-        BigInt denominador = a._denominador * b._denominador;
-        return Fracao(numerador, denominador);
-    }
-
-    friend Fracao operator/(const Fracao &a, const Fracao &b) {
-        BigInt numerador = a._numerador * b._denominador;
-        BigInt denominador = a._denominador * b._numerador;
-        return Fracao(numerador, denominador);
-    }
-
-    friend std::ostream& operator<<(std::ostream &out, const Fracao &f) {
-        if (f._denominador == BigInt(1)) {
-            out << f._numerador;
-        } else {
-            out << f._numerador << "/" << f._denominador;
-        }
+    friend std::ostream &operator<<(std::ostream &out, const C &c) {
+        c.print(out);
         return out;
     }
 };
 
+class R : public C {
+public:
+    R(double r = 0.0) : C(r, 0.0) {}
+
+    void print(std::ostream &out) const override {
+        out << getReal();
+    }
+};
+
+class Q : public R {
+private:
+    int _numerador;
+    int _denominador;
+
+    void reduz() {
+        int gcd = std::gcd(_numerador, _denominador);
+        _numerador /= gcd;
+        _denominador /= gcd;
+        if (_denominador < 0) {
+            _numerador = -_numerador;
+            _denominador = -_denominador;
+        }
+    }
+
+public:
+    Q(int num = 0, int den = 1) : R(static_cast<double>(num) / den), _numerador(num), _denominador(den) {
+        if (den == 0) {
+            throw std::invalid_argument("_denominador nao pode ser zero");
+        }
+        reduz();
+    }
+
+    int getNumerador() const { return _numerador; }
+    int getDenominador() const { return _denominador; }
+
+    void print(std::ostream &out) const override {
+        out << _numerador << "/" << _denominador;
+    }
+};
+
+class Z : public Q {
+public:
+    Z(int num = 0) : Q(num, 1) {}
+
+    void print(std::ostream &out) const override {
+        out << getNumerador();
+    }
+};
+
+class N : public Z {
+public:
+    N(int num = 0) : Z(num) {
+        if (num < 0) {
+            throw std::invalid_argument("num natural nao pode ser negativo");
+        }
+    }
+};
+
 int main() {
-    Fracao f;
-    std::cout << f << std::endl;
-    f = f + Fracao(BigInt(1), BigInt(2));
-    std::cout << f << std::endl;
-    f = f + Fracao(BigInt(1), BigInt(2));
-    std::cout << f << std::endl;
-    f = f * Fracao(BigInt(3), BigInt(2));
-    std::cout << f << std::endl;
-    f = f / Fracao(BigInt(3), BigInt(4));
-    std::cout << f << std::endl;
-    f = f - Fracao(BigInt(1), BigInt(6));
-    std::cout << f << std::endl;
-    f = f * Fracao(BigInt(0), BigInt("999999999999999"));
-    std::cout << f << std::endl;
+    const int numObjetos = 5;
+    C* objetos[numObjetos];
+    objetos[0] = new C(3.5, 2.1);
+    objetos[1] = new R(5.0);
+    objetos[2] = new Q(1, 3);
+    objetos[3] = new Z(10);
+    objetos[4] = new N(10);
+
+    for (int i = 0; i < numObjetos; ++i)
+        std::cout << *objetos[i] << std::endl;
+
+    for (int i = 0; i < numObjetos; ++i)
+        delete objetos[i];
 
     return 0;
 }
