5eab44bdcf14cd078445b1d21bdc7d46c8cbccbb
Joao <victoralmeida71@usp.br>
2024-11-05 08:42:57 -0300

    ex7

---
+++ lista02/ex6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdio.h>
+
+class BigInt {
+
+};
+
+int main() {
+  
+
+    return 0;
+}
--- lista02/ex7.cpp
+++ lista02/ex7.cpp
@@ -44,12 +44,12 @@ public:
 
 class Fracao {
 private:
-    BigInt numerador;
-    BigInt denominador;
+    BigInt _numerador;
+    BigInt _denominador;
 
 public:
     // Constructor with default values
-    Fracao(BigInt numerador = 0, BigInt denominador = 1) : numerador(numerador), denominador(denominador) {}
+    Fracao(BigInt _numerador = 0, BigInt _denominador = 1) : _numerador(_numerador), _denominador(_denominador) {}
 
     // Friend functions for arithmetic operations
     friend Fracao operator+(const Fracao& a, const Fracao& b);
@@ -63,31 +63,31 @@ public:
 
 // Implementations of friend functions
 Fracao operator+(const Fracao& a, const Fracao& b) {
-    BigInt new_numerador = a.numerador * b.denominador + b.numerador * a.denominador;
-    BigInt new_denominador = a.denominador * b.denominador;
+    BigInt new_numerador = a._numerador * b._denominador + b._numerador * a._denominador;
+    BigInt new_denominador = a._denominador * b._denominador;
     return Fracao(new_numerador, new_denominador);
 }
 
 Fracao operator-(const Fracao& a, const Fracao& b) {
-    BigInt new_numerador = a.numerador * b.denominador - b.numerador * a.denominador;
-    BigInt new_denominador = a.denominador * b.denominador;
+    BigInt new_numerador = a._numerador * b._denominador - b._numerador * a._denominador;
+    BigInt new_denominador = a._denominador * b._denominador;
     return Fracao(new_numerador, new_denominador);
 }
 
 Fracao operator*(const Fracao& a, const Fracao& b) {
-    BigInt new_numerador = a.numerador * b.numerador;
-    BigInt new_denominador = a.denominador * b.denominador;
+    BigInt new_numerador = a._numerador * b._numerador;
+    BigInt new_denominador = a._denominador * b._denominador;
     return Fracao(new_numerador, new_denominador);
 }
 
 Fracao operator/(const Fracao& a, const Fracao& b) {
-    BigInt new_numerador = a.numerador * b.denominador;
-    BigInt new_denominador = a.denominador * b.numerador;
+    BigInt new_numerador = a._numerador * b._denominador;
+    BigInt new_denominador = a._denominador * b._numerador;
     return Fracao(new_numerador, new_denominador);
 }
 
 std::ostream& operator<<(std::ostream &out, const Fracao &a) {
-    out << a.numerador << '/' << a.denominador;
+    out << a._numerador << '/' << a._denominador;
     return out;
 }
 
