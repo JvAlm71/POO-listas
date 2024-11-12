f0369864e988823af8f56642a4a34aabe53739d2
Joao <victoralmeida71@usp.br>
2024-11-12 15:23:49 -0300

    aaaaa

--- lista02/ex7.cpp
+++ lista02/ex7.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 
-// Classe BigInt simplificada
+// Classe BigInt
 class BigInt {
 private:
     long long int value;
@@ -42,27 +42,20 @@ public:
         return a;
     }
 
-    // Função para simplificar a fração
-    void simplify(BigInt& numerator, BigInt& denominator) const {
-        long long int gcdValue = gcd(numerator.getValue(), denominator.getValue());
-        numerator = numerator.getValue() / gcdValue;
-        denominator = denominator.getValue() / gcdValue;
-    }
-
     friend std::ostream& operator<<(std::ostream& out, const BigInt& num) {
         out << num.value;
         return out;
     }
 };
 
-// Classe Fracao com simplificação
+// Classe Fracao
 class Fracao {
 private:
     BigInt _numerador;
     BigInt _denominador;
 
 public:
-    // Construtor com validação de denominador
+    // Construtor com validação do denominador
     Fracao(BigInt numerador = 0, BigInt denominador = 1) : _numerador(numerador), _denominador(denominador) {
         if (_denominador == BigInt(0)) {
             std::cerr << "Erro: o denominador não pode ser zero" << std::endl;
@@ -71,7 +64,51 @@ public:
         simplify();
     }
 
-    // Função para simplificar a fração
+    // Operadores friend de adição, subtração, multiplicação e divisão
+    friend Fracao operator+(const Fracao& a, const Fracao& b) {
+        BigInt num = a._numerador * b._denominador + b._numerador * a._denominador;
+        BigInt den = a._denominador * b._denominador;
+        return Fracao::simplifyFraction(num, den);
+    }
+
+    friend Fracao operator-(const Fracao& a, const Fracao& b) {
+        BigInt num = a._numerador * b._denominador - b._numerador * a._denominador;
+        BigInt den = a._denominador * b._denominador;
+        return Fracao::simplifyFraction(num, den);
+    }
+
+    friend Fracao operator*(const Fracao& a, const Fracao& b) {
+        BigInt num = a._numerador * b._numerador;
+        BigInt den = a._denominador * b._denominador;
+        return Fracao::simplifyFraction(num, den);
+    }
+
+    friend Fracao operator/(const Fracao& a, const Fracao& b) {
+        BigInt num = a._numerador * b._denominador;
+        BigInt den = a._denominador * b._numerador;
+        return Fracao::simplifyFraction(num, den);
+    }
+
+    // Operador de saída
+    friend std::ostream& operator<<(std::ostream& out, const Fracao& f) {
+        out << f._numerador << "/" << f._denominador;
+        return out;
+    }
+
+private:
+    // Função para simplificar a fração (privada e estática)
+    static Fracao simplifyFraction(BigInt num, BigInt den) {
+        long long int gcdValue = BigInt::gcd(num.getValue(), den.getValue());
+        num = BigInt(num.getValue() / gcdValue);
+        den = BigInt(den.getValue() / gcdValue);
+        if (den.getValue() < 0) {  // Manter o denominador positivo
+            num = BigInt(-num.getValue());
+            den = BigInt(-den.getValue());
+        }
+        return Fracao(num, den);
+    }
+
+    // Simplificação inicial do construtor
     void simplify() {
         long long int gcdValue = BigInt::gcd(_numerador.getValue(), _denominador.getValue());
         _numerador = BigInt(_numerador.getValue() / gcdValue);
@@ -80,48 +117,6 @@ public:
             _numerador = BigInt(-_numerador.getValue());
             _denominador = BigInt(-_denominador.getValue());
         }
-    }
-
-    // Operador de adição
-    friend Fracao operator+(const Fracao& a, const Fracao& b) {
-        BigInt num = a._numerador * b._denominador + b._numerador * a._denominador;
-        BigInt den = a._denominador * b._denominador;
-        Fracao result(num, den);
-        result.simplify();
-        return result;
-    }
-
-    // Operador de subtração
-    friend Fracao operator-(const Fracao& a, const Fracao& b) {
-        BigInt num = a._numerador * b._denominador - b._numerador * a._denominador;
-        BigInt den = a._denominador * b._denominador;
-        Fracao result(num, den);
-        result.simplify();
-        return result;
-    }
-
-    // Operador de multiplicação
-    friend Fracao operator*(const Fracao& a, const Fracao& b) {
-        BigInt num = a._numerador * b._numerador;
-        BigInt den = a._denominador * b._denominador;
-        Fracao result(num, den);
-        result.simplify();
-        return result;
-    }
-
-    // Operador de divisão
-    friend Fracao operator/(const Fracao& a, const Fracao& b) {
-        BigInt num = a._numerador * b._denominador;
-        BigInt den = a._denominador * b._numerador;
-        Fracao result(num, den);
-        result.simplify();
-        return result;
-    }
-
-    // Operador de saída
-    friend std::ostream& operator<<(std::ostream& out, const Fracao& f) {
-        out << f._numerador << "/" << f._denominador;
-        return out;
     }
 };
 
